3631a801cbd4d0dc7b7623484b1ccec3c1d13bf8
Viacheslav Glazhevskyi <glajik@gmail.com>
2017-08-10 16:30:08 +0200

    10. Двухмерные массивы. Задание 1 Готово

--- 10. Двухмерные массивы/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ 10. Двухмерные массивы/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        // Вычислить среднее арифметическое матрицы
+
         static void Main(string[] args)
         {
             int LengthR = -1, LengthC = -1;
@@ -51,6 +53,42 @@ namespace ConsoleApplication1
                 if (!valid) Console.WriteLine("Повторите еще раз, это должно быть число от 2 до 10.");
             }
             while (!valid);
+
+            Console.Clear();
+
+            Console.WriteLine();
+            Console.WriteLine("Вывод матрицы, заполненной случайными числами:");
+            Console.WriteLine();
+
+            int[,] Matrix = new int[LengthR,LengthC];
+
+            Random rnd = new Random();
+
+            int summ = 0;
+            int num = 0;
+            double SrArifm = 0.0;
+
+            for (int indexR = 0; indexR < LengthR; indexR++)
+            {
+                for (int indexC = 0; indexC < LengthC; indexC++)
+                {
+                    Matrix[indexR, indexC] = rnd.Next(1, 10);
+                    Console.Write("{0: 0 }", Matrix[indexR, indexC]);
+
+                    num++;
+                    summ+=Matrix[indexR, indexC];
+                }
+                Console.WriteLine(); 
+                Console.WriteLine();
+            };
+            Console.WriteLine();
+
+            SrArifm = (double)summ / num;
+
+            Console.WriteLine("Среднее арифметическое матрицы: {0,3}", SrArifm);
+
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть окно.");
+            Console.ReadKey();
         }
     }
 }
