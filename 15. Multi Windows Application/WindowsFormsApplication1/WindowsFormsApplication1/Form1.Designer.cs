6c7bcde27e36cb96e1880c524225c56ab7e591bd
Viacheslav Glazhevskyi <22841201+Glajik@users.noreply.github.com>
2017-09-02 22:32:47 +0200

    Game have a base functionality..

--- 15. Multi Windows Application/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ 15. Multi Windows Application/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -65,6 +65,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -310,6 +311,7 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.listView2);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -403,6 +405,14 @@
             this.listView1.TabIndex = 3;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(7, 52);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(146, 295);
+            this.listView2.TabIndex = 5;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -437,7 +447,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Memories, The";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -484,6 +494,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
     }
 }
 
--- 15. Multi Windows Application/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ 15. Multi Windows Application/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -61,8 +61,10 @@ namespace WindowsFormsApplication1
             int coin = rnd.Next(1);
             if (coin == 0) groupBox1.Enabled = true;
             else groupBox2.Enabled = true;
-            
-            
+
+            //init Listview
+            listView1.LargeImageList = imageList2;
+            listView2.LargeImageList = imageList2;
         }
 
 
@@ -77,9 +79,21 @@ namespace WindowsFormsApplication1
                 {
                     firstButton.Hide();
                     secondButton.Hide();
+
+                    if (groupBox1.Enabled)
+                    {
+
+                        listView1.Items.Add("", firstCard);
+
+                    }
+                    else
+                    {
+                        listView2.Items.Add("", firstCard);
+                    }
                 }
                 
                 firstCard = secondCard = -1;
+
                 button1.Image = null;
                 button2.Image = null;
                 button3.Image = null;
@@ -105,6 +119,31 @@ namespace WindowsFormsApplication1
                 button23.Image = null;
                 button24.Image = null;
 
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                button7.Enabled = true;
+                button8.Enabled = true;
+                button9.Enabled = true;
+                button10.Enabled = true;
+                button11.Enabled = true;
+                button12.Enabled = true;
+                button13.Enabled = true;
+                button14.Enabled = true;
+                button15.Enabled = true;
+                button16.Enabled = true;
+                button17.Enabled = true;
+                button18.Enabled = true;
+                button19.Enabled = true;
+                button20.Enabled = true;
+                button21.Enabled = true;
+                button22.Enabled = true;
+                button23.Enabled = true;
+                button24.Enabled = true;
+
                 groupBox1.Enabled = !groupBox1.Enabled;
                 groupBox2.Enabled = !groupBox2.Enabled;
             }
@@ -116,11 +155,13 @@ namespace WindowsFormsApplication1
             {
                 firstCard = image_index;
                 firstButton = this_button;
+                firstButton.Enabled = false;
             }
             else if (secondCard == -1)
             {
                 secondCard = image_index;
                 secondButton = this_button;
+                secondButton.Enabled = false;
             }
 
             CardForm.BackgroundImage = imageList1.Images[image_index];
@@ -148,276 +189,107 @@ namespace WindowsFormsApplication1
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[3];
-            else if (secondCard == -1) secondCard = img_id[3];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[3]];
-            CardForm.Show();
-
-            button4.Image = imageList2.Images[img_id[3]];
-
-            timer1.Enabled = true;
+            ShowCard(button4, img_id[3]);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[4];
-            else if (secondCard == -1) secondCard = img_id[4];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[4]];
-            CardForm.Show();
-
-            button5.Image = imageList2.Images[img_id[4]];
-
-            timer1.Enabled = true;
+            ShowCard(button5, img_id[4]);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[5];
-            else if (secondCard == -1) secondCard = img_id[5];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[5]];
-            CardForm.Show();
-
-            button6.Image = imageList2.Images[img_id[5]];
-
-            timer1.Enabled = true;
+            ShowCard(button6, img_id[5]);
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[6];
-            else if (secondCard == -1) secondCard = img_id[6];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[6]];
-            CardForm.Show();
-
-            button7.Image = imageList2.Images[img_id[6]];
-
-            timer1.Enabled = true;
+            ShowCard(button7, img_id[6]);
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[7];
-            else if (secondCard == -1) secondCard = img_id[7];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[7]];
-            CardForm.Show();
-
-            button8.Image = imageList2.Images[img_id[7]];
-
-            timer1.Enabled = true;
+            ShowCard(button8, img_id[7]);
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[8];
-            else if (secondCard == -1) secondCard = img_id[8];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[8]];
-            CardForm.Show();
-
-            button9.Image = imageList2.Images[img_id[8]];
-
-            timer1.Enabled = true;
+            ShowCard(button9, img_id[8]);
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[9];
-            else if (secondCard == -1) secondCard = img_id[9];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[9]];
-            CardForm.Show();
-
-            button10.Image = imageList2.Images[img_id[9]];
-
-            timer1.Enabled = true;
+            ShowCard(button10, img_id[9]);
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[10];
-            else if (secondCard == -1) secondCard = img_id[10];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[10]];
-            CardForm.Show();
-
-            button11.Image = imageList2.Images[img_id[10]];
-
-            timer1.Enabled = true;
-
+            ShowCard(button11, img_id[10]);
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[11];
-            else if (secondCard == -1) secondCard = img_id[11];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[11]];
-            CardForm.Show();
-
-            button12.Image = imageList2.Images[img_id[11]];
-
-            timer1.Enabled = true;
+            ShowCard(button12, img_id[11]);
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[12];
-            else if (secondCard == -1) secondCard = img_id[12];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[12]];
-            CardForm.Show();
-
-            button13.Image = imageList2.Images[img_id[12]];
-
-            timer1.Enabled = true;
+            ShowCard(button13, img_id[12]);
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[13];
-            else if (secondCard == -1) secondCard = img_id[13];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[13]];
-            CardForm.Show();
-
-            button14.Image = imageList2.Images[img_id[13]];
-
-            timer1.Enabled = true;
+            ShowCard(button14, img_id[13]);
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[14];
-            else if (secondCard == -1) secondCard = img_id[14];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[14]];
-            CardForm.Show();
-
-            button15.Image = imageList2.Images[img_id[14]];
-
-            timer1.Enabled = true;
+            ShowCard(button15, img_id[14]);
         }
 
         private void button16_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[15];
-            else if (secondCard == -1) secondCard = img_id[15];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[15]];
-            CardForm.Show();
-
-            button16.Image = imageList2.Images[img_id[15]];
-
-            timer1.Enabled = true;
+            ShowCard(button16, img_id[15]);
         }
 
         private void button17_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[16];
-            else if (secondCard == -1) secondCard = img_id[16];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[16]];
-            CardForm.Show();
-
-            button17.Image = imageList2.Images[img_id[16]];
-
-            timer1.Enabled = true;
+            ShowCard(button17, img_id[16]);
         }
 
         private void button18_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[17];
-            else if (secondCard == -1) secondCard = img_id[17];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[17]];
-            CardForm.Show();
-
-            button18.Image = imageList2.Images[img_id[17]];
-
-            timer1.Enabled = true;
+            ShowCard(button18, img_id[17]);
         }
 
         private void button19_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[18];
-            else if (secondCard == -1) secondCard = img_id[18];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[18]];
-            CardForm.Show();
-
-            button19.Image = imageList2.Images[img_id[18]];
-
-            timer1.Enabled = true;
+            ShowCard(button19, img_id[18]);
         }
 
         private void button20_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[19];
-            else if (secondCard == -1) secondCard = img_id[19];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[19]];
-            CardForm.Show();
-
-            button20.Image = imageList2.Images[img_id[19]];
-
-            timer1.Enabled = true;
+            ShowCard(button20, img_id[19]);
         }
 
         private void button21_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[20];
-            else if (secondCard == -1) secondCard = img_id[20];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[20]];
-            CardForm.Show();
-
-            button21.Image = imageList2.Images[img_id[20]];
-
-            timer1.Enabled = true;
+            ShowCard(button21, img_id[20]);
         }
 
         private void button22_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[21];
-            else if (secondCard == -1) secondCard = img_id[21];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[21]];
-            CardForm.Show();
-
-            button22.Image = imageList2.Images[img_id[21]];
-
-            timer1.Enabled = true;
+            ShowCard(button22, img_id[21]);
         }
 
         private void button23_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[22];
-            else if (secondCard == -1) secondCard = img_id[22];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[22]];
-            CardForm.Show();
-
-            button23.Image = imageList2.Images[img_id[22]];
-
-            timer1.Enabled = true;
+            ShowCard(button23, img_id[22]);
         }
 
         private void button24_Click(object sender, EventArgs e)
         {
-            if (firstCard == -1) firstCard = img_id[23];
-            else if (secondCard == -1) secondCard = img_id[23];
-
-            CardForm.BackgroundImage = imageList1.Images[img_id[23]];
-            CardForm.Show();
-
-            button24.Image = imageList2.Images[img_id[23]];
-
-            timer1.Enabled = true;
+            ShowCard(button24, img_id[23]);
         }
 
     }
