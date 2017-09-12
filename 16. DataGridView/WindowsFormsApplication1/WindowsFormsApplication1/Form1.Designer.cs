34d7a1ef962db86508b467b80b99e6cb3be1d406
Viacheslav Glazhevskyi <22841201+Glajik@users.noreply.github.com>
2017-09-13 00:10:32 +0200

    16. DataGridView - simple currency calculator

---
+++ 16. DataGridView/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Currency = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rates = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Incoming = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Commission = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your money, $:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Commission, %:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Currency,
+            this.Rates,
+            this.Incoming,
+            this.Commission,
+            this.Result});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(568, 310);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Exchange";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "10";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(123, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "2";
+            // 
+            // Currency
+            // 
+            this.Currency.HeaderText = "Currency";
+            this.Currency.Name = "Currency";
+            // 
+            // Rates
+            // 
+            this.Rates.HeaderText = "Exchange Rates";
+            this.Rates.Name = "Rates";
+            // 
+            // Incoming
+            // 
+            this.Incoming.HeaderText = "Incoming";
+            this.Incoming.Name = "Incoming";
+            // 
+            // Commission
+            // 
+            this.Commission.HeaderText = "Commission";
+            this.Commission.Name = "Commission";
+            // 
+            // Result
+            // 
+            this.Result.HeaderText = "Result";
+            this.Result.Name = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 439);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Convert curencies";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Currency;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rates;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Incoming;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Commission;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Result;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ 16. DataGridView/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        double rate_eur = 0.83575;
+        double rate_gbp = 0.75295;
+        double rate_chf = 0.95985;
+        double rate_pln = 3.55883;
+        double rate_uah = 26.0730;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Rows.Add(4);
+            dataGridView1.Rows[0].Cells[0].Value = "EUR - Euro";
+            dataGridView1.Rows[0].Cells[1].Value = rate_eur.ToString();
+
+            dataGridView1.Rows[1].Cells[0].Value = "GBP - British Pound";
+            dataGridView1.Rows[1].Cells[1].Value = rate_gbp.ToString();
+
+            dataGridView1.Rows[2].Cells[0].Value = "CHF - Swiss Franc";
+            dataGridView1.Rows[2].Cells[1].Value = rate_chf.ToString();
+
+            dataGridView1.Rows[3].Cells[0].Value = "PLN - Polish Zloty";
+            dataGridView1.Rows[3].Cells[1].Value = rate_pln.ToString();
+
+            dataGridView1.Rows[4].Cells[0].Value = "UAH - Ukrainian Hrivna";
+            dataGridView1.Rows[4].Cells[1].Value = rate_uah.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(textBox1.Text);
+            double commission = Convert.ToDouble(textBox2.Text);
+            double fee, result;
+
+            fee = amount * rate_eur / 100 * commission;
+            result = amount * rate_eur - fee;
+            dataGridView1.Rows[0].Cells[2].Value = amount.ToString();
+            dataGridView1.Rows[0].Cells[3].Value = fee.ToString();
+            dataGridView1.Rows[0].Cells[4].Value = result.ToString();
+
+            fee = amount * rate_gbp / 100 * commission;
+            result = amount * rate_gbp - fee;
+            dataGridView1.Rows[1].Cells[2].Value = amount.ToString();
+            dataGridView1.Rows[1].Cells[3].Value = fee.ToString();
+            dataGridView1.Rows[1].Cells[4].Value = result.ToString();
+
+            fee = amount * rate_chf / 100 * commission;
+            result = amount * rate_chf - fee;
+            dataGridView1.Rows[2].Cells[2].Value = amount.ToString();
+            dataGridView1.Rows[2].Cells[3].Value = fee.ToString();
+            dataGridView1.Rows[2].Cells[4].Value = result.ToString();
+
+            fee = amount * rate_pln / 100 * commission;
+            result = amount * rate_pln - fee;
+            dataGridView1.Rows[3].Cells[2].Value = amount.ToString();
+            dataGridView1.Rows[3].Cells[3].Value = fee.ToString();
+            dataGridView1.Rows[3].Cells[4].Value = result.ToString();
+
+            fee = amount * rate_uah / 100 * commission;
+            result = amount * rate_uah - fee;
+            dataGridView1.Rows[4].Cells[2].Value = amount.ToString();
+            dataGridView1.Rows[4].Cells[3].Value = fee.ToString();
+            dataGridView1.Rows[4].Cells[4].Value = result.ToString();
+        }
+    }
+}
