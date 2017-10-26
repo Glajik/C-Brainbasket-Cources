cd9f659bb4b50d8813cdb5a4052ed8899ca9f24c
Viacheslav Glazhevskyi <22841201+Glajik@users.noreply.github.com>
2017-10-26 17:56:28 +0300

    17. Graphics. Ex.1 is OK.

--- 17. Графика/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ 17. Графика/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -25,35 +25,62 @@ namespace WindowsFormsApplication1
                 public int height;
             }
             
-            sWindow window;
+            static int resolution = 100;
+            static int periods = 2;
+            public double[] plotArray = new double[resolution * periods]; // array for old way to store points
+            public int last_i = resolution * periods - 1; // it for array
 
-            static int max_points_at_period = 100;
-            static int count_periods = 2;
-            public double[] plotArray = new double[max_points_at_period * count_periods];
-            public System.Drawing.Point[] plotArraySDP = new System.Drawing.Point[max_points_at_period * count_periods];
-            public int last_i = max_points_at_period * count_periods - 1;
-
+            public System.Drawing.Point[] plotArraySDP = new System.Drawing.Point[resolution * periods]; // object for modern way to do the same
 
 
-            public void SinX(sWindow window)
+            /* There is calculate and save in arrays dots of selected function.
+             * Function is receive size of drawing window and string from ComboBox, 
+             * where user select what kind of function he want to see.
+             * I think, it better solve in this case to re-use code with initial value and cycle.
+             * It more readable. But not optimal perfomance, because every itteration do the switch and a few function calls.
+             */ 
+            public void Draw(sWindow my_window, string selected_func)
             {
-                int amplitude = window.height;
-                int x0 = window.top;
-                int y0 = window.left;
-                int x1 = window.width;
-                int y1 = window.height;
-                float y_center = (this.Height - uptab - downtab) / 2 + uptab;  
-
+                double amplitude = my_window.height / 4; // height of waves
+                double tab_y = (double)my_window.height / 2 + my_window.top; // offset to center of window
+                double dx = (double)my_window.width / (resolution * periods); // scale of increment coordinate x
+                double arg = 1; // argument of function
+                double f = 1; // result of function
+                double x = my_window.left; // coordinate x for drawing graphic
                 for (int i = 0; i <= last_i; i++)
                 {
-            
-                    plotArray[i] = amplitude * Math.Sin(2 * Math.PI / max_points_at_period * i);
-                    plotArraySDP[i].X = i;
-                    plotArraySDP[i].Y = (int)(amplitude * Math.Sin(2 * Math.PI / max_points_at_period * i));
+                    arg = 2 * Math.PI / resolution * i;
+
+                    switch (selected_func)
+                    {
+                        case "1. Sin (x)": { f = Math.Sin(arg); break; }
+                        case "2. Cos (x)": { f = Math.Cos(arg); break; }
+                        case "3. Sin (x) + Sin (2x)": { f = Math.Sin(arg) + Math.Sin(2 * arg); break; }
+                        case "4. Sin (x) - Sin (2x)": { f = (Math.Sin(arg) - Math.Sin(2 * arg)); break; }
+                        case "5. Sin (x) + Cos (2x)": { f = (Math.Sin(arg) + Math.Cos(2 * arg)); break; }
+                        case "6. Sin (x) - Cos (2x)": { f = (Math.Sin(arg) - Math.Cos(2 * arg)); break; }
+                        case "7. Sin (x) * Exp (x)": { f = (Math.Sin(arg) * Math.Exp(arg)); amplitude = 0.01;  break; }
+                        case "8. Cos (x) * Exp (x)": { f = (Math.Cos(arg) * Math.Exp(arg)); amplitude = 0.01;  break; }
+                        case "9. Sin (x) * Exp (-x)": { f = (Math.Sin(arg) * Math.Exp(-arg)); amplitude = my_window.height*1.25; break; }
+                        case "10.  Cos (x) * Exp (-x)": { f = (Math.Cos(arg) * Math.Exp(-arg)); amplitude = my_window.height/2; break; }
+                    }
+
+                    // classic array - old way                    
+                    plotArray[i] = amplitude * f + tab_y;
+
+                    // object - new way
+                    plotArraySDP[i].X = (int)x;
+                    plotArraySDP[i].Y = (int)(amplitude * f + tab_y);
+
+                    x += dx;
                 }
             }
+
+
+
+
         }
-        
+
         // Get handle of main window
         Graphics hwnd;
 
@@ -126,33 +153,44 @@ namespace WindowsFormsApplication1
             hwnd.DrawLine(pen_axis, x0, y_center, x1, y_center);
             hwnd.DrawLine(pen_axis, x0, y0, x0, y1);
 
-            int imax = 100, t = 2; // число точек в периоде и число периодов.
-            double[] f = new double[imax*t + 10];
+            
+            cPlotFunction.sWindow my_window; // var for struct that is contain coordinates of drawing field 
+            my_window.left = lefttab;
+            my_window.top = uptab;
+            my_window.width = this.Width - lefttab - righttab;
+            my_window.height = this.Height - uptab - downtab;
+            
 
-            cPlotFunction.sWindow window;
-            window.left = lefttab;
-            window.top = uptab;
-            window.width = this.Width - lefttab - righttab;
-            window.height = this.Height - uptab - downtab;
+            // Draw selected function in ComboBox
+            PlotFunction.Draw(my_window, comboBox1.SelectedItem.ToString());
 
-            // Draw selected function
-            switch (comboBox1.SelectedItem.ToString()) {
-                case "1. Sin (x)": { PlotFunction.SinX(window); break; }
-                /*case "2. Cos (x)":
-                case "3. Sin (x) + Sin (2x)":
-                case "4. Sin (x) - Sin (2x)":
-                case "5. Sin (x) + Cos (2x)":
-                case "6. Sin (x) - Cos (2x)":
-                case "7. Sin (x) * Exp (x)":
-                case "8. Cos (x) * Exp (x)":
-                case "9. Sin (x) * Exp (-x)":
-                case "10.  Cos (x) * Exp (-x)": */
-            }
 
-               hwnd.DrawLines(pen_plotter, PlotFunction.plotArraySDP);
+            // render graphic in old way. You can uncomment and see that is the same
+            //Classic_Render(x0, x1, PlotFunction);
+
+            // render graphic to new way   
+             hwnd.DrawLines(pen_plotter, PlotFunction.plotArraySDP);
+
         }
 
+        // render graphic of selected function
+        private void Classic_Render(Pen pen_plotter, float x0, float x1, cPlotFunction PlotFunction)
+        {
+            double x_last = x0;
+            double y_last = PlotFunction.plotArray[0];
+            double dx = (x1 - x0) / (100 * 2);
+            double x = x_last;
 
+            foreach (double y in PlotFunction.plotArray)
+            {
+                // pass the first itteration, because we want get coords only for start point now
+                if (x != x_last) hwnd.DrawLine(pen_plotter, (int)x_last, (int)y_last, (int)x, (int)y);
+                y_last = y;
+                x_last = x;
+                x += dx;
+
+            }
+        }
         private void button2_Click(object sender, EventArgs e)
         {
             Application.Exit();
