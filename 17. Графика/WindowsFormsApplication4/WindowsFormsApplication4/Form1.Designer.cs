c5bea4ae4207bdc084bf3815f87abf48e6e0af5c
Viacheslav Glazhevskyi <22841201+Glajik@users.noreply.github.com>
2017-11-14 13:56:18 +0200

    Ex.4 Simple Graphic Converter.

--- 17. Графика/WindowsFormsApplication4/WindowsFormsApplication4/Form1.Designer.cs
+++ 17. Графика/WindowsFormsApplication4/WindowsFormsApplication4/Form1.Designer.cs
@@ -115,7 +115,8 @@
             "JPEG",
             "PNG",
             "GIF",
-            "TIFF"});
+            "TIFF",
+            "BMP"});
             this.comboBox1.Location = new System.Drawing.Point(460, 476);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(106, 21);
--- 17. Графика/WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
+++ 17. Графика/WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
@@ -13,7 +13,13 @@ namespace WindowsFormsApplication4
 {
     public partial class Form1 : Form
     {
-        MemoryStream memStream;
+        private MemoryStream memStream;
+        private FileStream sourceFile = null;
+        private JpegBitmapEncoder encJpeg;
+        private PngBitmapEncoder encPng;
+        private GifBitmapEncoder encGif;
+        private TiffBitmapEncoder encTiff;
+        private BmpBitmapEncoder encBmp;
 
         public Form1()
         {
@@ -25,36 +31,39 @@ namespace WindowsFormsApplication4
             openFileDialog1.FileName = "";
             openFileDialog1.RestoreDirectory = true;
 
-            saveFileDialog1.Filter = "JPEG files *.jpeg|PNG files *.png|GIF files *.gif|TIFF files *.tiff";
+            saveFileDialog1.Filter = "JPEG files (*.jpg)|*.jpg|PNG files (*.png)|*.png|GIF files (*.gif)|*.gif|TIFF files (*.tif)|*.tif";
             comboBox1.Enabled = false;
             numericUpDown1.Enabled = false;
             comboBox3.Enabled = false;
-
         }
 
         // Show openFileDialog and load picture to PictureBox
         private void button1_Click(object sender, EventArgs e)
         {
-            Stream myStream = null;
-
             DialogResult result = openFileDialog1.ShowDialog();
             if (result == DialogResult.OK)
             {
                 try
                 {
-                    myStream = openFileDialog1.OpenFile();
-                    if (myStream != null)
+                    // close previous opened file
+                    if (sourceFile != null)
+                    {
+                        sourceFile.Close();
+                        sourceFile = null;
+                    }
+                    
+                    // open selected file
+                    sourceFile = new FileStream(openFileDialog1.FileName, FileMode.Open);
+
+                    if (sourceFile != null)
                     {
                         // Show path, name and size in KB
-                        label1.Text = openFileDialog1.FileName + " - " + (myStream.Length / 1024) + " KBytes";
+                        label1.Text = openFileDialog1.FileName + " - " + (sourceFile.Length / 1024) + " KBytes";
                         // Show picture
-                        pictureBox1.Image = Image.FromStream(myStream);
-                        // Close file
-                        myStream.Close();
+                        pictureBox1.Image = Image.FromStream(sourceFile);
                         comboBox1.Enabled = true;
                         numericUpDown1.Enabled = false;
                         comboBox3.Enabled = false;
-
                     }
                 }
                 catch (Exception ex)
@@ -62,15 +71,31 @@ namespace WindowsFormsApplication4
                     comboBox1.Enabled = false;
                     numericUpDown1.Enabled = false;
                     comboBox3.Enabled = false;
-                    MessageBox.Show("Something wrong. -> " + ex.Message);
+                    MessageBox.Show("Error: Couldn't open selected file. -> " + ex.Message);
                 }
             }
 
         }
 
+        // Write result to file
         private void button2_Click(object sender, EventArgs e)
         {
-            saveFileDialog1.ShowDialog();
+            saveFileDialog1.FileName = "";
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    FileStream resultFile = new FileStream(saveFileDialog1.FileName, FileMode.Create);
+                    memStream.Position = 0;
+                    memStream.WriteTo(resultFile);
+                    resultFile.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Couldn't save to file. -> " + ex.Message);
+                }
+            }
         }
 
         // Let user to change SizeMode for a detailed view
@@ -86,45 +111,12 @@ namespace WindowsFormsApplication4
             }
         }
 
-        // Direction to the corresponding function according to the user's choice
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+        // Free old, and allocate new piece of memory
+        private void RenewMemStream()
         {
-            // pre-config controls
-            switch (comboBox1.SelectedItem.ToString())
-            {
-                case "JPEG":
-                    comboBox1.Enabled = true;
-                    numericUpDown1.Enabled = true;
-                    comboBox3.Enabled = false;
-                    break;
-                case "PNG":
-                    comboBox1.Enabled = true;
-                    numericUpDown1.Enabled = false;
-                    comboBox3.Enabled = false;
-                    break;
-                case "GIF":
-                    comboBox1.Enabled = true;
-                    numericUpDown1.Enabled = false;
-                    comboBox3.Enabled = false;
-                    break;
-                case "TIFF":
-                    comboBox1.Enabled = true;
-                    numericUpDown1.Enabled = false;
-                    comboBox3.Enabled = true;
-                    break;
-                default:
-                    comboBox1.Enabled = true;
-                    numericUpDown1.Enabled = false;
-                    comboBox3.Enabled = false;
-                    break;
-            }
-
-
-
-            //Make preview match to selected format
             try
             {
-                FileStream openfile = new FileStream(openFileDialog1.FileName, FileMode.Open);
                 // if it's first time to allocate memory
                 if (memStream == null) memStream = new MemoryStream();
                 // free memory
@@ -133,85 +125,240 @@ namespace WindowsFormsApplication4
                     memStream.Close();
                     memStream = new MemoryStream();
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
-                if (openfile != null)
+        // Try to show preview - result of compression
+        private void ShowPreview()
+        {
+            try
+            {
+                if (memStream.Capacity != 0)
                 {
-                    switch (comboBox1.SelectedItem.ToString())
-                    {
-                        case "JPEG":
-                            JpegBitmapEncoder encJpeg = new JpegBitmapEncoder();
-                            encJpeg.QualityLevel = (int)numericUpDown1.Value;
-                            encJpeg.Frames.Add(BitmapFrame.Create(openfile));
-                            encJpeg.Save(memStream);
-                            break;
-                        case "PNG":
-                            PngBitmapEncoder encPng = new PngBitmapEncoder();
-                            encPng.Frames.Add(BitmapFrame.Create(openfile));
-                            encPng.Save(memStream);
-                            break;
-                        case "GIF":
-                            GifBitmapEncoder encGif = new GifBitmapEncoder();
-                            encGif.Frames.Add(BitmapFrame.Create(openfile));
-                            encGif.Save(memStream);
-                            break;
-                        case "TIFF":
-                            TiffBitmapEncoder encTiff = new TiffBitmapEncoder();
-                            encTiff.Compression = TiffCompressOption.Default;
-                            if (comboBox3.SelectedItem == null) comboBox3.SelectedIndex = 1;
-                            switch (comboBox3.SelectedItem.ToString())
-                            {
-                                case "None":
-                                    encTiff.Compression = TiffCompressOption.None;
-                                    break;
-                                case "Default":
-                                    encTiff.Compression = TiffCompressOption.Default;
-                                    break;
-                                case "Lwz":
-                                    encTiff.Compression = TiffCompressOption.Lzw;
-                                    break;
-                                case "Rle":
-                                    encTiff.Compression = TiffCompressOption.Rle;
-                                    break;
-                                case "Zip":
-                                    encTiff.Compression = TiffCompressOption.Zip;
-                                    break;
-                                default:
-                                    MessageBox.Show("Please, select a proper type of compression from a list");
-                                    encTiff.Compression = TiffCompressOption.Default;
-                                    break;
-                            }
-                            encTiff.Frames.Add(BitmapFrame.Create(openfile));
-                            encTiff.Save(memStream);
-                            break;
-                        default:
-                            MessageBox.Show("Please, select graphic format from a list");
-                            BmpBitmapEncoder encBmp = new BmpBitmapEncoder();
-                            encBmp.Frames.Add(BitmapFrame.Create(openfile));
-                            encBmp.Save(memStream);
-                            break;
-                    }
-
                     label2.Text = "Result: size - " + (memStream.Length / 1024) + " KBytes";
                     pictureBox2.Image = Image.FromStream(memStream);
-                    //memStream.WriteTo(tempfile);
-                    openfile.Close();
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error: Coulud not write file. " + ex.Message);
+                MessageBox.Show("Error: " + ex.Message);
             }
         }
 
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        private void EncodeToBmp()
         {
-            comboBox1_SelectedIndexChanged(sender, e);
+            RenewMemStream();
+
+            try
+            {
+                if (sourceFile != null)
+                {
+                    sourceFile.Position = 0;
+                    encBmp = new BmpBitmapEncoder();
+                    encBmp.Frames.Add(BitmapFrame.Create(sourceFile));
+                    encBmp.Save(memStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
 
+        private void EncodeToTiff(TiffCompressOption mode)
+        {
+            RenewMemStream();
+
+            try
+            {
+                if (sourceFile != null)
+                {
+                    sourceFile.Position = 0;
+                    encTiff = new TiffBitmapEncoder();
+                    encTiff.Frames.Add(BitmapFrame.Create(sourceFile));
+                    encTiff.Compression = mode;
+                    encTiff.Save(memStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void EncodeToGif()
+        {
+            RenewMemStream();
+
+            try
+            {
+                if (sourceFile != null)
+                {
+                    sourceFile.Position = 0;
+                    encGif = new GifBitmapEncoder();
+                    encGif.Frames.Add(BitmapFrame.Create(sourceFile));
+                    encGif.Save(memStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void EncodeToPng()
+        {
+            RenewMemStream();
+
+            try
+            {
+                if (sourceFile != null)
+                {
+                    sourceFile.Position = 0;
+                    encPng = new PngBitmapEncoder();
+                    encPng.Frames.Add(BitmapFrame.Create(sourceFile));
+                    encPng.Save(memStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void EncodeToJpeg(decimal quality)
+        {
+            RenewMemStream();
+
+            try
+            {
+                if (sourceFile != null)
+                {
+                    sourceFile.Position = 0;
+                    encJpeg = new JpegBitmapEncoder();
+                    encJpeg.Frames.Add(BitmapFrame.Create(sourceFile));
+                    encJpeg.QualityLevel = (int)quality;
+                    encJpeg.Save(memStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        // Handle of changing percent of quality for Jpeg
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            EncodeToJpeg(numericUpDown1.Value);
+            ShowPreview();
+        }
+
+        // Handle of change format type
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                // pre-config controls
+                comboBox1.Enabled = true;
+                numericUpDown1.Enabled = false;
+                comboBox3.Enabled = false;
+
+                switch (comboBox1.SelectedItem.ToString())
+                {
+                    case "JPEG":
+                        numericUpDown1.Enabled = true; // Jpeg Quality
+                        EncodeToJpeg(numericUpDown1.Value);
+                        ShowPreview();
+                        saveFileDialog1.Filter = "JPEG files (*.jpg)|*.jpg";
+                        break;
+
+                    case "PNG":
+                        EncodeToPng();
+                        ShowPreview();
+                        saveFileDialog1.Filter = "PNG files (*.png)|*.png";
+                        break;
+
+                    case "GIF":
+                        EncodeToGif();
+                        ShowPreview();
+                        saveFileDialog1.Filter = "GIF files (*.gif)|*.gif";
+                        break;
+
+                    case "TIFF":
+                        comboBox3.Enabled = true; // Tiff compression mode
+                        comboBox3_SelectedIndexChanged(sender, e);
+                        // Functions EncodeToTiff() & ShowPreview will calling in comboBox_SelectedIndexChanged();
+                        saveFileDialog1.Filter = "TIFF files (*.tif)|*.tif";
+                        break;
+
+                    case "BMP":
+                        EncodeToBmp();
+                        ShowPreview();
+                        saveFileDialog1.Filter = "BMP files (*.bmp)|*.bmp";
+                        break;
+
+                    default:
+                        MessageBox.Show("Please, select graphic format from a list");
+                        break;
+                }
+            }
+        }
+
+        // handle of changing compression mode for Tiff
         private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
-            comboBox1_SelectedIndexChanged(sender, e);
+            try
+            {
+                if (comboBox3.SelectedItem != null)
+                {
+                    switch (comboBox3.SelectedItem.ToString())
+                    {
+                        case "None":
+                            EncodeToTiff(TiffCompressOption.None);
+                            ShowPreview();
+                            break;
+                        case "Default":
+                            EncodeToTiff(TiffCompressOption.Default);
+                            ShowPreview();
+                            break;
+                        case "Lwz":
+                            EncodeToTiff(TiffCompressOption.Lzw);
+                            ShowPreview();
+                            break;
+                        case "Rle":
+                            EncodeToTiff(TiffCompressOption.Rle);
+                            ShowPreview();
+                            break;
+                        case "Zip":
+                            EncodeToTiff(TiffCompressOption.Zip);
+                            ShowPreview();
+                            break;
+                        default:
+                            MessageBox.Show("Please, select a proper type of compression from a list");
+                            EncodeToTiff(TiffCompressOption.Default);
+                            ShowPreview();
+                            break;
+                    }
+                }
+                else
+                {
+                    comboBox3.SelectedItem = "None";
+                    EncodeToTiff(TiffCompressOption.None);
+                    ShowPreview();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
+
+
 
     }
 }
